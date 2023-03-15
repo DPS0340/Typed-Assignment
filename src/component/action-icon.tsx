23983f685b974227c8ccb562e52b84971589450b
Jiho Lee <optional.int@kakao.com>
2023-03-15 19:48:17 +0900

    feat: improve ActionIcon

--- src/component/action-icon.tsx
+++ src/component/action-icon.tsx
@@ -1,10 +1,11 @@
 import { TypedIcon, TypedColor } from "typed-design-system";
 import { ActionIconParams } from "../type/action-icon";
 
-export const ActionIcon = ({ icon, onClick }: ActionIconParams) => {
+export const ActionIcon = (params: ActionIconParams) => {
+  const { onClick, className } = params;
   return (
-    <div className="flex flex-grow" onClick={onClick}>
-      <TypedIcon icon={icon} color={TypedColor.fromHex("#000000")} />
+    <div onClick={onClick} className={className}>
+      <TypedIcon color={TypedColor.fromHex("#000000")} {...params} />
     </div>
   );
 };
